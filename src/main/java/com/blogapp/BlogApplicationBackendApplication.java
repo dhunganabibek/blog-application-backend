3a0f464987aaf75decd11b9b47d4d3304986c6c1
dhunganabibek <Dhunganabibek2016@gmail.com>
2023-02-07 21:06:51 -0600

    initial setup

---
+++ src/main/java/com/blogapp/BlogApplicationBackendApplication.java
@@ -0,0 +1,13 @@
+package com.blogapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlogApplicationBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlogApplicationBackendApplication.class, args);
+	}
+
+}
